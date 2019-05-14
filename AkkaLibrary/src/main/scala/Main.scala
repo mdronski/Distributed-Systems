e3167e3628f04cfa5b3eb17d45bdcc0d9ec84732
mdronski <dronskimichal@gmail.com>
2019-05-14 16:55:47 +0200

    Akka - work done

--- AkkaLibrary/src/main/scala/Main.scala
+++ AkkaLibrary/src/main/scala/Main.scala
@@ -1,20 +1,29 @@
-import java.util.concurrent.TimeUnit
+import actors.MainActor
+import akka.actor.{ActorSystem, Props}
+import database.operations.{Find, Order, StreamText}
 
-import actors.{FindActor, TextStreamActor}
-import akka.actor.{Actor, ActorSystem, Props}
-import akka.stream.ActorMaterializer
-import database.operations.{Find, StreamText}
-
-import scala.concurrent.Await
-import scala.concurrent.duration.Duration
-
+import scala.io.StdIn
 
 object Main extends App {
   val system: ActorSystem = ActorSystem("HelloSystem")
+  val mainActor = system.actorOf(Props[MainActor], "mainActor")
+  val search = "s (.+)".r
+  val order = "o (.+)".r
+  val textStream = "t (.+)".r
 
-  val helloActor = system.actorOf(Props[TextStreamActor], "findActor")
-  helloActor ! new StreamText("commonTitle")
-  Await.ready(system.whenTerminated, Duration(1, TimeUnit.MINUTES))
-  system.stop(helloActor)
 
+  println("Operations:")
+  println("s <title>     //searches for given title in db and print its price")
+  println("o <title>     //place an order for a given title")
+  println("t <title>     //print content of a given title, line by line in 1 second intervals")
+  println("x             // shutdown application")
+
+  Iterator.continually(StdIn.readLine())
+    .takeWhile(_ != "x")
+    .foreach {
+      case search(title) => mainActor ! new Find(title)
+      case order(title) => mainActor ! new Order(title)
+      case textStream(title) => mainActor ! new StreamText(title)
+      case _ => println("Incorrect command")
+    }
 }
